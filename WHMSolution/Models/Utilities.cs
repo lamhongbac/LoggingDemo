1c9a7a0573a72b6bbbd4013c2b7e4c31f276c692
lam hong bac <lamhong.bac@ms-apptech.com>
2022-08-22 20:35:40 +0700

    update code

---
+++ WHMSolution/Models/Utilities.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Http;
+using OfficeOpenXml;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WHMSolution.Models
+{
+    public class Utilities
+    {
+        public static async Task ImportMasterItemBarCode(IFormFile file)
+        {
+
+            List<MobMasterStockModel> list = new List<MobMasterStockModel>();
+            StringBuilder sb = new StringBuilder();
+            using (var stream = new MemoryStream())
+            {
+                await file.CopyToAsync(stream);
+                ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+                using (var package = new ExcelPackage(stream))
+                {
+                    ExcelWorksheet worksheet = package.Workbook.Worksheets[0];
+                    var rowcount = worksheet.Dimension.Rows;
+
+                    #region string builder HTML
+                    sb.Append("<table class='table table-bordered'><tr>");
+                    List<string> columnNames = new List<string>();
+                    foreach (var firstRowCell in worksheet.Cells[worksheet.Dimension.Start.Row, worksheet.Dimension.Start.Column, 1, worksheet.Dimension.End.Column])
+                    {
+                        columnNames.Add(firstRowCell.Text);
+                        sb.Append("<th>" + firstRowCell.Text.ToString() + "</th>");
+                    }
+                    sb.Append("</tr>");
+                    sb.AppendLine("<tr>");
+                    var start = worksheet.Dimension.Start;
+                    var end = worksheet.Dimension.End;
+                    for (int i = start.Row + 1; i <= end.Row; i++)
+                    { // Row by row...
+                        for (int col = start.Column; col <= end.Column; col++)
+                        { // ... Cell by cell...
+                            object cellValue = worksheet.Cells[i, col].Text; // This got me the actual value I needed.
+                            sb.Append("<td>" + cellValue.ToString() + "</td>");
+                        }
+                        sb.AppendLine("</tr>");
+                    }
+                    sb.Append("</table>");
+
+
+                    #endregion
+
+                    for (int row = 2; row <= rowcount; row++)
+                    {
+                        list.Add(new MobMasterStockModel
+                        {
+                            BarCode = worksheet.Cells[row, 1].Value.ToString().Trim(),
+                            Name = worksheet.Cells[row, 2].Value.ToString().Trim(),
+                            Unit = worksheet.Cells[row, 3].Value.ToString().Trim(),
+                            Description = worksheet.Cells[row, 4].Value.ToString().Trim()
+                        });
+                    }
+
+                }
+            }
+        }
+    }
+}
--- WHMSolution/Pages/StockMaster/Import.cshtml.cs
+++ WHMSolution/Pages/StockMaster/Import.cshtml.cs
@@ -1,37 +1,80 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using WHMSolution.Models;
+using RazorAPP.Data;
+
 
 namespace WHMSolution.Pages.StockMaster
 {
 
     public class ImportModel : PageModel
     {
-        private IHostingEnvironment _hostingEnv;
+        private IHostingEnvironment _hostenvironment;
         public ImportModel(IHostingEnvironment hostingEnv)
         {
-            _hostingEnv = hostingEnv;
+            _hostenvironment = hostingEnv;
         }
         public void OnGet()
         {
         }
         public FileViewModel FileUpload { get; set; }
+
         public async Task<ActionResult> OnPostAsync()
         {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+            //upload file to folder
             if (FileUpload.FormFile.Length > 0)
             {
-
+                using (var stream = new FileStream(Path.Combine(_hostenvironment.WebRootPath, "uploadfiles", FileUpload.FormFile.FileName), FileMode.Create))
+                {
+                    await FileUpload.FormFile.CopyToAsync(stream);
+                }
             }
+            //save image to database.
+            using (var memoryStream = new MemoryStream())
+            {
+                await FileUpload.FormFile.CopyToAsync(memoryStream);
+
+                // Upload the file if less than 2 MB
+                if (memoryStream.Length < 2097152)
+                {
+                    var file = new AppFile()
+                    {
+                        FileName = FileUpload.FormFile.FileName,
+                        Content = memoryStream.ToArray()
+                    };
+                    _context.File.Add(file);
+
+                    await _context.SaveChangesAsync();
+
+                }
+                else
+                {
+                    ModelState.AddModelError("File", "The file is too large.");
+                }
+            }
+            return RedirectToPage("./Index");
 
         }
     }
     public class FileViewModel
     {
         public IFormFile FormFile { get; set; }
+    }
+    public class AppFile
+    {
+        public int Id { get; set; }
+        public string FileName { get; set; }
+        public byte[] Content { get; set; }
     }
 }

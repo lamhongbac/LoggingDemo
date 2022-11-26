e6e398b1dec66f772718d890eb704400f3f4fd8b
IT Director <lamhong.bac@gmail.com>
2022-11-26 22:28:11 +0700

    update Signal R study

---
+++ SignalRChat/Hubs/ChatHub.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignalRChat.Hubs
+{
+    public class ChatHub : Hub
+    {
+        //https://learn.microsoft.com/en-us/aspnet/core/tutorials/signalr?source=recommendations&view=aspnetcore-5.0&tabs=visual-studio
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+    }
+}

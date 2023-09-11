13d6ae7f8fa29cfe6de7fbcfecfb790e12e5b2e6
lam hong bac <lamhong.bac@gmail.com>
2023-09-11 18:06:44 +0700

    update code

---
+++ MVCWeb/Controllers/TransactionController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCWeb.Helper.PopUp;
+using MVCWeb.Models;
+using System.Collections.Generic;
+
+namespace MVCWeb.Controllers
+{
+    public class TransactionController : Controller
+    {
+        public IActionResult Index()
+        {
+            DemoData demoData = new DemoData();
+            List<TransactionViewModel> model = demoData.CreateListData();
+            return View(model);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(TransactionViewModel model)
+        {
+            return View(model);
+        }
+    }
+}
---
+++ MVCWeb/Helper/PopUp/DemoData.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper.Execution;
+using MVCWeb.Models;
+using System.Collections.Generic;
+
+namespace MVCWeb.Helper.PopUp
+{
+    public class DemoData
+    {
+        public List<TransactionViewModel> CreateListData()
+        {
+            List<TransactionViewModel> datas = new List<TransactionViewModel>();
+            for (int i = 0;i<5; i++)
+            {
+                TransactionViewModel data = new TransactionViewModel()
+                {
+                    Amount = i * 1000,
+                    Bank = "VCB",
+                    ID = i + 1,
+                    Name = " Le van Nuoi " + i.ToString(),
+                    Number = "10222" + i.ToString(),
+                    SWIFTCode = (10222 + i).ToString()
+                };
+                datas.Add(data);
+            }
+            return datas;
+        }
+    }
+}

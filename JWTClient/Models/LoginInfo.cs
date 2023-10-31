ecb07a797f167e2fe7ea2173504ada581fe86c5a
lam hong bac <lamhong.bac@gmail.com>
2023-10-31 16:04:30 +0700

    update code

---
+++ JWTClient/Models/LoginInfo.cs
@@ -0,0 +1,25 @@
+﻿namespace JWTClient.Models
+{
+    public class UserInfo
+    {
+        public string ID { get; set; }
+        public string UserName { get; set; }
+        public string FullName { get; set; }
+        public string EmailAddress { get; set; }
+        public string Roles { get; set; }
+    }
+    public class LoginInfo
+    {
+        public LoginInfo()
+        {
+            LoginDate = DateTime.Now;
+            JwtData = new JwtData();
+
+        }
+
+        public DateTime LoginDate { get; set; }
+        //example cho truong hop user data tong quat duoi dang string object
+
+        public JwtData JwtData { get; set; }
+    }
+}
--- JWTClient/Services/AccountService.cs
+++ JWTClient/Services/AccountService.cs
@@ -1,24 +1,77 @@
 ﻿using JWTClient.Models;
+using Newtonsoft.Json;
+using System;
+using System.Security.Claims;
+using System.Security.Policy;
+using System.Text;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+
+
+
+using Microsoft.AspNetCore.Mvc;
+
+using System.Reflection;
+using System.Security.Principal;
 
 namespace JWTClient.Services
 {
     public class AccountService
     {
+        IHttpContextAccessor _httpContextAccessor;
+        HttpClient _httpClient;
+     const   string  strUrl= "";
+        public AccountService(HttpClient httpClient, IHttpContextAccessor httpContextAccessor)
+        {
+            _httpClient = httpClient;
+            _httpContextAccessor= httpContextAccessor;
+        }
         /// <summary>
         /// 
         /// </summary>
         /// <param name="username"></param>
         /// <param name="password"></param>
         /// <returns></returns>
-        public JwtData Login(string username, string password)
+        public async Task Login(LoginModel model)
         {
+           
+
             // qua trinh login cua MVC
             //Cap nhat cookie information
+            string json = JsonConvert.SerializeObject(model);
+            StringContent data = new StringContent(json, Encoding.UTF8, "application/json");
+            var response = await _httpClient.PostAsync(strUrl, data);
+            string result = await response.Content.ReadAsStringAsync();
+            LoginInfo loginInfo = JsonConvert.DeserializeObject<LoginInfo> (result);
+
+            //save cookier JwtData
+            JwtUtil jwtUtil= new JwtUtil(); 
+            UserInfo userInfo = jwtUtil.GetUserInfo(loginInfo.JwtData.Jwt);
+
+            List<Claim> claims = new List<Claim>()
+            {
+                    new Claim(ClaimTypes.NameIdentifier,userInfo.FullName),
+                    new Claim("Roles", string.Join(",", userInfo.Roles)),
+                    new Claim(ClaimTypes.Email,userInfo.EmailAddress),
+
+                //new Claim(ClaimTypes.StreetAddress,account.Address)
+            };
+            ClaimsIdentity identity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+
+            AuthenticationProperties properties = new AuthenticationProperties()
+            {
+                AllowRefresh = true,
+                IsPersistent = model.KeepLogined
+            };
+
+            await _httpContextAccessor.HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme,
+                new ClaimsPrincipal(identity), properties);
         }
 
         public JwtData ReNewToken(JwtData jwtData)
         {
             //Cap nhat cookie information
+            return new JwtData();
         }
     }
 }
